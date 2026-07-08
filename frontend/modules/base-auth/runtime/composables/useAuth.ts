75253e5fdafca6545d073f1afcced53b75013f33
bubaley <bubaley.fu@gmail.com>
2026-07-09 00:54:27 +0200

    ref: remove comment string

--- frontend/modules/base-auth/runtime/composables/useAuth.ts
+++ frontend/modules/base-auth/runtime/composables/useAuth.ts
@@ -103,7 +103,6 @@ export const useAuth = () => {
     'base-auth-session',
     async () => {
       const { tokens } = await getTokens()
-      console.log('tokens', tokens, 'shouldRefresh', shouldRefresh(tokens, config.sessionRefresh.beforeExpiry))
       if (shouldRefresh(tokens, config.sessionRefresh.beforeExpiry)) await refresh()
       if (!state.value.tokens.access?.isValid) {
         return {
